339d1767bcb722a8c878a755f275ab3fb90b7c48
Diego Arrieta <diego.arrietacalle@outlook.com>
2025-03-20 17:53:06 -0500

    Add map example

--- CivilAPI/Example.cs
+++ CivilAPI/Example.cs
@@ -5,18 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.DatabaseServices;
+
+using AcadApp = Autodesk.AutoCAD.ApplicationServices;
+using AcadDB = Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Geometry;
+using AcadGeo = Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+
 using Autodesk.Aec.PropertyData.DatabaseServices;
-using Civil = Autodesk.Civil.DatabaseServices;
+
+using Autodesk.Gis.Map.Platform;
+using Autodesk.Gis.Map.Platform.Interop;
+using Autodesk.Gis.Map.Platform.Internal;
+using OSGeo.MapGuide;
+
 using CivilApp = Autodesk.Civil.ApplicationServices;
+using CivilDB = Autodesk.Civil.DatabaseServices;
+
 using CivilAPI.Extensions;
 using CivilAPI.Forms;
 using CivilAPI.Wrappers;
-using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using Autodesk.AutoCAD.LayerManager;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows.Forms;
+using System.Data;
 
 namespace CivilAPI
 {
@@ -25,19 +37,19 @@ namespace CivilAPI
         [CommandMethod("LayoutFromObject")]
         public void LayoutFromObject()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
-            Entity entity = document.PickEntity();
-            Layout layout = entity.GetLayout();
+            AcadDB.Entity entity = document.PickEntity();
+            AcadDB.Layout layout = entity.GetLayout();
             editor.WriteMessage(layout.LayoutName);
         }
         [CommandMethod("FlattenPolyline")]
         public void FlattenPolyline()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
             //Polyline3d polyline3d = document.PickEntityOfType("POLYLINE") as Polyline3d;
@@ -50,8 +62,8 @@ namespace CivilAPI
         [CommandMethod("FormExample")]
         public void FormExample()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
             PropertySetsForm propertySetsForm = new PropertySetsForm(document, database, editor);
@@ -60,24 +72,24 @@ namespace CivilAPI
         [CommandMethod("ExampleProgam")]
         public void ExampleProgam()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
-            Autodesk.AutoCAD.DatabaseServices.Entity entity = document.PickEntity();
-            Layout layout = entity.GetLayout();
+            AcadDB.Entity entity = document.PickEntity();
+            AcadDB.Layout layout = entity.GetLayout();
 
             database.Run(tr =>
             {
-                BlockTableRecord blockTableRecord = tr.GetObject(layout.BlockTableRecordId, OpenMode.ForRead) as BlockTableRecord;
-                blockTableRecord.CreateLine(new Point3d(0, 0, 0), new Point3d(10, 10, 0));
+                AcadDB.BlockTableRecord blockTableRecord = tr.GetObject(layout.BlockTableRecordId, AcadDB.OpenMode.ForRead) as AcadDB.BlockTableRecord;
+                blockTableRecord.CreateLine(new AcadGeo.Point3d(0, 0, 0), new AcadGeo.Point3d(10, 10, 0));
             });
         }
         [CommandMethod("SelectOnlyPolys")]
         public void SelectOnlyPolys()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
             document.PickEntitiesOfType("CIRCLE");
@@ -85,21 +97,21 @@ namespace CivilAPI
         [CommandMethod("SelectPipeNetwork")]
         public void SelectPipeNetwork()
         {
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
             Editor editor = document.Editor;
 
-            Civil.Pipe pipe = document.PickEntityOfType("AECC_PIPE") as Civil.Pipe;
+            CivilDB.Pipe pipe = document.PickEntityOfType("AECC_PIPE") as CivilDB.Pipe;
 
             database.Run(tr =>
             {
-                Civil.Network pipeNetwork = tr.GetObject(pipe.NetworkId, OpenMode.ForRead) as Civil.Network;
-     
+                CivilDB.Network pipeNetwork = tr.GetObject(pipe.NetworkId, AcadDB.OpenMode.ForRead) as CivilDB.Network;
+                     
                 var structureIdCollection = pipeNetwork.GetStructureIds();
 
-                foreach (ObjectId structureId in structureIdCollection)
+                foreach (AcadDB.ObjectId structureId in structureIdCollection)
                 {
-                    Civil.Structure structure = tr.GetObject(structureId, OpenMode.ForWrite) as Civil.Structure;
+                    CivilDB.Structure structure = tr.GetObject(structureId, AcadDB.OpenMode.ForWrite) as CivilDB.Structure;
 
                     double pipeLowest = structure.PipeLowestBottomDepth;
                     double rimElevation = structure.RimElevation;
@@ -109,9 +121,45 @@ namespace CivilAPI
                     {
                         structure.SumpElevation = rimElevation - pipeLowest;
                         editor.WriteMessage($"{structure.Name} | {sumpElevation.ToString()} => {structure.SumpElevation.ToString()}\n");
-                    }                                        
+                    }
                 }
             });
+        }
+        [CommandMethod("InitMap")]
+        public void InitMap()
+        {
+            AcadApp.Document document = AcadApp.Application.DocumentManager.MdiActiveDocument;
+            AcadDB.Database database = document.Database;
+            Editor editor = document.Editor;
+
+            AcMapMap currentMap = AcMapMap.GetCurrentMap();
+            MgLayerCollection layers = currentMap.GetLayers();
+            AcMapLayer layer = layers.First() as AcMapLayer;
+
+            AcadGeo.Point3d point = new AcadGeo.Point3d();
+            point = editor.EnterPoint();
+
+            MgGeometryFactory geometryFactory = new MgGeometryFactory();
+            MgCoordinate coordinate = geometryFactory.CreateCoordinateXY(point.X, point.Y);
+            MgPoint mgPoint = geometryFactory.CreatePoint(coordinate);
+
+            //MgWktReaderWriter writer = new MgWktReaderWriter();
+            //string wktPoint = writer.Write(mgPoint);
+            //editor.WriteMessage($"{wktPoint}\n");
+
+            MgAgfReaderWriter agfReaderWriter = new MgAgfReaderWriter();
+            MgByteReader byteReader = agfReaderWriter.Write(mgPoint);
+
+            MgPropertyCollection propertyCollection = new MgPropertyCollection();
+            propertyCollection.Add(new MgGeometryProperty(layer.GetFeatureGeometryName(), byteReader));
+            propertyCollection.Add(new MgStringProperty("STNAME", "Autodesk"));
+
+            MgFeatureCommandCollection featureCommandCollection = new MgFeatureCommandCollection();
+            featureCommandCollection.Add(new MgInsertFeatures(layer.GetFeatureClassName(), propertyCollection));
+            layer.UpdateFeatures(featureCommandCollection);
+
+            layer.SaveFeatureChanges(new MgFeatureQueryOptions());
+            layer.ForceRefresh();
         }
     }
 }
